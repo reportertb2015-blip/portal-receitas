52d930f1ead4b5762bdfeacfd5fccf77599e3c8b
reportertb2015-blip <reportertb2015@gmail.com>
2026-05-12 21:25:16 -0300

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,6 @@
-export const SITE = {
+import type { Site, SocialObjects } from "./types";
+
+export const SITE: Site = {
   website: "https://curadanatureza.receitagratis.com/", 
   author: "Anderson Kochanski",
   profile: "https://receitagratis.com/", 
@@ -16,8 +18,48 @@ export const SITE = {
     text: "Sugerir alteração",
     url: "https://github.com/reportertb2015-blip/portal-receitas/edit/main/",
   },
-  dynamicOgImage: true,
-  dir: "ltr", 
-  lang: "pt-BR", 
-  timezone: "America/Sao_Paulo", 
 } as const;
+
+export const LOCALE = {
+  mainOutdated: "pt-BR", 
+} as const;
+
+export const LOGO_IMAGE = {
+  enable: false,
+  svg: true,
+  width: 216,
+  height: 46,
+};
+
+export const SOCIALS: SocialObjects = [
+  {
+    name: "Github",
+    href: "https://github.com/reportertb2015-blip/portal-receitas",
+    linkTitle: `Repositório do Anderson`,
+    active: true,
+  },
+];
+
+// ESTA É A PARTE QUE FAZ O MENU APARECER NO ASTROPAPER:
+export const navLinks = [
+  {
+    href: "/posts",
+    linkText: "Receitas",
+  },
+  {
+    href: "/categories",
+    linkText: "Categorias",
+  },
+  {
+    href: "/tags",
+    linkText: "Cura Natureza",
+  },
+  {
+    href: "/about",
+    linkText: "Sobre",
+  },
+  {
+    href: "/search",
+    linkText: "Pesquisar",
+  },
+];
